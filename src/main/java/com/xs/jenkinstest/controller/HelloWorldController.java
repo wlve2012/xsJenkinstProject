788f8e7e2ff9b4750dc584bffdde18973b264c0a
wlve2012 <1784613203@qq.com>
2023-03-09 18:58:46 +0800

    Initial commit

---
+++ src/main/java/com/xs/jenkinstest/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.xs.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkins")
+public class HelloWorldController {
+    @GetMapping("hello")
+    public String get(){
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/xs/jenkinstest/JenkinstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xs.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
